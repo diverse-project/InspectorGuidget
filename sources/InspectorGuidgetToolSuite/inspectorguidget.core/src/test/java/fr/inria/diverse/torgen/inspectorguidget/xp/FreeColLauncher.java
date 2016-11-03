c4ac23e52be524cbb2725ea3752fae1074b10459
arnobl <blouin.arno@gmail.com>
2016-11-03 17:31:11 +0100

    fix(xp): freecol config updated

--- sources/InspectorGuidgetToolSuite/inspectorguidget.core/src/test/java/fr/inria/diverse/torgen/inspectorguidget/xp/FreeColLauncher.java
+++ sources/InspectorGuidgetToolSuite/inspectorguidget.core/src/test/java/fr/inria/diverse/torgen/inspectorguidget/xp/FreeColLauncher.java
@@ -6,36 +6,39 @@ import java.util.List;
 public class FreeColLauncher extends XPLauncher {
 
 	public static void main(String args[]) {
+		// git clone git://git.code.sf.net/p/freecol/git freecol-git
+		// git checkout v0.11.6
+		// Modify the constructor of CaptureGoodsDialog to let the Java code loadable by Spoon.
 		new FreeColLauncher().run();
 	}
 
 	@Override
 	protected List<String> getInputResoures() {
-		return Collections.singletonList("/media/data/dev/repoAnalysisBlob/FreeCol/src");
+		return Collections.singletonList("/media/data/dev/repoAnalysisBlob/freecol-git/src");
 	}
 
 	@Override
 	protected String[] getSourceClassPath() {
-		return new String[]{"/media/data/dev/repoAnalysisBlob/FreeCol/jars/commons-cli-1.1.jar",
-			"/media/data/dev/repoAnalysisBlob/FreeCol/jars/cortado-0.6.0.jar",
-			"/media/data/dev/repoAnalysisBlob/FreeCol/jars/jogg-0.0.7.jar",
-			"/media/data/dev/repoAnalysisBlob/FreeCol/jars/jorbis-0.0.15.jar",
-			"/media/data/dev/repoAnalysisBlob/FreeCol/jars/miglayout-core-4.2.jar",
-			"/media/data/dev/repoAnalysisBlob/FreeCol/jars/miglayout-swing-4.2.jar"};
+		return new String[]{"/media/data/dev/repoAnalysisBlob/freecol-git/jars/commons-cli-1.1.jar",
+			"/media/data/dev/repoAnalysisBlob/freecol-git/jars/cortado-0.6.0.jar",
+			"/media/data/dev/repoAnalysisBlob/freecol-git/jars/jogg-0.0.17.jar",
+			"/media/data/dev/repoAnalysisBlob/freecol-git/jars/jorbis-0.0.17.jar",
+			"/media/data/dev/repoAnalysisBlob/freecol-git/jars/miglayout-core-4.2.jar",
+			"/media/data/dev/repoAnalysisBlob/freecol-git/jars/miglayout-swing-4.2.jar"};
 	}
 
 	@Override
 	protected int getCompilianceLevel() {
-		return 6;
+		return 8;
 	}
 
 	@Override
 	protected boolean usingLambda() {
-		return false;
+		return true;
 	}
 
 	@Override
 	protected String getOutputFolder() {
-		return "/media/data/dev/refactor/freecolRefactor";
+		return "/media/data/dev/refactor/freecol-refactored/src";
 	}
 }
