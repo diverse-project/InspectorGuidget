68f2078a6d9586a276d1d06c958363865b03a7bb
arnobl <blouin.arno@gmail.com>
2016-12-05 18:21:18 +0100

    fixed(analysis): false positive dispatch methods when embedded in if conds

--- sources/InspectorGuidgetToolSuite/inspectorguidget.core/src/main/java/fr/inria/diverse/torgen/inspectorguidget/analyser/Command.java
+++ sources/InspectorGuidgetToolSuite/inspectorguidget.core/src/main/java/fr/inria/diverse/torgen/inspectorguidget/analyser/Command.java
@@ -150,7 +150,7 @@ public class Command {
 											map(stat -> stat.getElements(new ClassMethodCallFilter(params, parent, false))).
 											flatMap(s -> s.stream()).collect(Collectors.toList());
 
-			if(invoks.size()==1 && main.getStatmts().size()==1 &&
+			if(invoks.size()==1 && main.getStatmts().size()==1 && main.getStatmts().get(0)==invoks.get(0) &&
 				invoks.get(0).isParentInitialized() && !(invoks.get(0).getParent() instanceof CtInvocation<?>)) {
 				final CtBlock<?> body = invoks.get(0).getExecutable().getDeclaration().getBody();
 				if(body!=null && !body.getStatements().isEmpty()) {
